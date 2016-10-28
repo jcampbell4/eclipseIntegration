c94fa4e2d31e26178edcba0dd0b90b57352cde7a
jcampbell4 <jcampbell4@CS-S121.iowa.uiowa.edu>
2016-10-28 10:55:05 -0500

    runable test

--- hellogit/src/hellogit/Helloworld.java
+++ hellogit/src/hellogit/Helloworld.java
@@ -2,7 +2,10 @@ package hellogit;
 
 public class Helloworld {
 	
-	public Helloworld(){
+	public static void say(){
 		System.out.println("Hello World");
+	}
+	public static void main(String [] args){
+		say();
 	}
 }
